96e833ebafcf42971d215aa549ee0f641e70f6b2
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2023-11-18 15:22:15 -0700

    Added a countdown feature for the app to track time till the actual movie premiere

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./input.css";
 import Header from "./Header";
 import HeroPage from "./Heropage";
+import Countdown from "./Countdown";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className=" app | w-[80vw] min-h-[100vh] py-[2rem]  ">
         <Header />
         <HeroPage className = "max-[640px]: flex justify-center flex-col"/>
+        <Countdown />
       </div>
     </div>
   );
--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,9 +1,46 @@
-const  Countdown = () => {
-  return ( 
-    <div>
-      
+import { useState, useRef, useEffect } from "react";
+const Countdown = () => {
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+  let interval = useRef();
+  const startTime = () => {
+    const countdownDate = new Date("December 26, 2023 00:00:00").getTime();
+    interval = setInterval(() => {
+      const now = newDate.getTime();
+      const difference = countdownDate - now;
+      const days = math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = math.floor(
+        difference / (1000 * 60 * 60 * 24) / (1000 * 60 * 60)
+      );
+      const minutes = math.floor(difference / (1000 * 60 * 60) / (1000 * 60));
+      const seconds = math.floor(difference / (1000 * 60) / 1000);
+
+      if (difference < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+
+    useEffect(() => {
+      startTime();
+      return () => {
+        clearInterval(interval.current);
+      };
+    });
+  };
+
+  return (
+    <div className="timer">
+      <span>{timerDays}</span>:<span>{timerHours}</span>:
+      <span>{timerMinutes}</span>:<span>{timerSeconds}</span>
     </div>
-   );
-}
- 
-export default  Countdown;
+  );
+};
+
+export default Countdown;
