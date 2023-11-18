f0ccb7bca2fd69ca517e984bdefb5d1450ba5d1e
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2023-11-18 15:33:47 -0700

    A typographical error in the math.floor in the logic aspect of the countdown, so I made corrections to them

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,39 +1,42 @@
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
+
 const Countdown = () => {
   const [timerDays, setTimerDays] = useState("00");
   const [timerHours, setTimerHours] = useState("00");
   const [timerMinutes, setTimerMinutes] = useState("00");
   const [timerSeconds, setTimerSeconds] = useState("00");
-  let interval = useRef();
+  const interval = useRef();
+
   const startTime = () => {
     const countdownDate = new Date("December 26, 2023 00:00:00").getTime();
-    interval = setInterval(() => {
-      const now = newDate.getTime();
+    interval.current = setInterval(() => {
+      const now = new Date().getTime();
       const difference = countdownDate - now;
-      const days = math.floor(difference / (1000 * 60 * 60 * 24));
-      const hours = math.floor(
-        difference / (1000 * 60 * 60 * 24) / (1000 * 60 * 60)
+
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       );
-      const minutes = math.floor(difference / (1000 * 60 * 60) / (1000 * 60));
-      const seconds = math.floor(difference / (1000 * 60) / 1000);
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
       if (difference < 0) {
         clearInterval(interval.current);
       } else {
-        setTimerDays(days);
-        setTimerHours(hours);
-        setTimerMinutes(minutes);
-        setTimerSeconds(seconds);
+        setTimerDays(days.toString().padStart(2, "0"));
+        setTimerHours(hours.toString().padStart(2, "0"));
+        setTimerMinutes(minutes.toString().padStart(2, "0"));
+        setTimerSeconds(seconds.toString().padStart(2, "0"));
       }
     }, 1000);
-
-    useEffect(() => {
-      startTime();
-      return () => {
-        clearInterval(interval.current);
-      };
-    });
   };
+
+  useEffect(() => {
+    startTime();
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, []);
 
   return (
     <div className="timer">
